db99676cc8d473f9bfaa1d07c2dcb4385f23a539
*vk-amogh* <*amogh.vk.2005@gmail.com*>
2026-03-04 13:30:11 +0530

    fix: use default import for gsap to fix Vercel SSR error

--- app/components/specular/flowing-menu/FlowingMenu.tsx
+++ app/components/specular/flowing-menu/FlowingMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { gsap } from 'gsap';
+import gsap from 'gsap';
 import { Scale, DollarSign, BatteryLow, TrendingDown, Wrench } from 'lucide-react';
 
 import './FlowingMenu.css';
